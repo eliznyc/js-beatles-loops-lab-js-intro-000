80b819f92d6f4c70cb812079a27010f61ef2f18b
Elizabeth Peters <eliznyc@gmail.com>
2019-08-30 01:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i < 4; i++) {band.push(musicians[i] + ` plays ` + instruments[i]) }
     return band;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var bigfacts = []
+  for (let i = 0; i < 4; i++) {bigfacts.push(facts[i] + `!!!`)}
+}
