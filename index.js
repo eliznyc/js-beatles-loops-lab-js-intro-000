a330913dce37e780d93ee7f0026efb8becb5c034
Elizabeth Peters <eliznyc@gmail.com>
2019-08-30 00:48:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  const musicans = ('john', 'paul', 'george', 'ringo')
-  const instruments = ('guitar', 'guitar', 'bass', 'drums')
+  const musicans = ('john', 'paul', 'george', 'ringo');
+  const instruments = ('guitar', 'guitar', 'bass', 'drums');
+  var band = [];
+
 }
